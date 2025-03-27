60eee3850a209c08eee49365f60e03ea433d6b54
abdulmoizniazi <abdulmoizniazi@gmail.com>
2025-03-28 03:31:08 +0500

    first commit

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+    <>
+    <h1>404</h1>
+    <div>the page you are looking for is NotFound</div>
+    </>
+  )
+}
+
+export default NotFound
